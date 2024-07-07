44d36efa87f4da1ddaf32887a61555019917ffdb
evgenia-fienik <160489180+evgenia-fienik@users.noreply.github.com>
2024-07-07 17:24:21 +0300

    css

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -33,10 +33,10 @@ function createImageCard({
     <img cass="img" src="${webformatURL}" alt="${tags}" loading="lazy"/>
     </a>
       <div class="info">
-        <p class="info-item"><b>Likes:</b>${likes}</p>
-        <p  class="info-item"><b>Views:</b>${views}</p>
-        <p class="info-item"><b>Comments:</b>${comments}</p>
-        <p class="info-item"><b>Downloads:</b>${downloads}</p>
+        <p class="info-item"><b>Likes </b>${likes}</p>
+        <p  class="info-item"><b>Views </b>${views}</p>
+        <p class="info-item"><b>Comments </b>${comments}</p>
+        <p class="info-item"><b>Downloads </b>${downloads}</p>
       </div>
 </div>`;
 }
