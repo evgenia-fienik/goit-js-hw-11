838ec97b2d4bdc0dc0cb07b695eeae23cf267e9b
evgenia-fienik <160489180+evgenia-fienik@users.noreply.github.com>
2024-07-10 18:41:47 +0300

    виправлення

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,15 +1,41 @@
+// const BASE_URL = 'https://pixabay.com/api/';
+// const API_KEY = '44774351-929c2aa0766411c652402d8c1';
+
+// export async function fetchImages(query, page = 1, perPage = 12) {
+//   const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(
+//     query
+//   )}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&perPage=${perPage}`;
+//   const response = await fetch(url);
+
+//   if (!response.ok) {
+//     throw new Error(response.status);
+//   }
+//   const data = await response.json();
+//   return data;
+// }
+
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '44774351-929c2aa0766411c652402d8c1';
 
-export async function fetchImages(query, page = 1, perPage = 12) {
+export function fetchImages(query, page = 1, perPage = 12) {
   const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(
     query
   )}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&perPage=${perPage}`;
-  const response = await fetch(url);
 
-  if (!response.ok) {
-    throw new Error(response.status);
-  }
-  const data = await response.json();
-  return data;
+  return (
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      // .then(data => {
+      //   return data;
+      // })
+      .catch(error => {
+        console.error(error);
+        throw error;
+      })
+  );
 }
--- src/main.js
+++ src/main.js
@@ -1,3 +1,52 @@
+// import { fetchImages } from './js/pixabay-api.js';
+// import {
+//   renderImages,
+//   showNotification,
+//   clearGallery,
+// } from './js/render-functions.js';
+
+// const form = document.querySelector('.search-form');
+// const input = document.querySelector('#search-input');
+// const loader = document.querySelector('.loader');
+
+// form.addEventListener('submit', async event => {
+//   event.preventDefault();
+//   const query = input.value.trim();
+//   if (!query) {
+//     showNotification('Please enter a search query.', 'warning');
+//     return;
+//   }
+//   clearGallery();
+//   showLoader();
+//   input.value = '';
+
+//   try {
+//     const data = await fetchImages(query);
+//     hideLoader();
+
+//     if (data.hits.length === 0) {
+//       showNotification(
+//         'Sorry, there are no images matching your search query. Please try again!',
+//         'error'
+//       );
+//       return;
+//     }
+
+//     renderImages(data.hits);
+//   } catch (error) {
+//     hideLoader();
+//     showNotification('Error: ${error.message}');
+//   }
+// });
+
+// function showLoader() {
+//   loader.classList.remove('hidden');
+// }
+
+// function hideLoader() {
+//   loader.classList.add('hidden');
+// }
+
 import { fetchImages } from './js/pixabay-api.js';
 import {
   renderImages,
@@ -9,7 +58,7 @@ const form = document.querySelector('.search-form');
 const input = document.querySelector('#search-input');
 const loader = document.querySelector('.loader');
 
-form.addEventListener('submit', async event => {
+form.addEventListener('submit', event => {
   event.preventDefault();
   const query = input.value.trim();
   if (!query) {
@@ -20,23 +69,22 @@ form.addEventListener('submit', async event => {
   showLoader();
   input.value = '';
 
-  try {
-    const data = await fetchImages(query);
-    hideLoader();
-
-    if (data.hits.length === 0) {
-      showNotification(
-        'Sorry, there are no images matching your search query. Please try again!',
-        'error'
-      );
-      return;
-    }
-
-    renderImages(data.hits);
-  } catch (error) {
-    hideLoader();
-    showNotification('Error: ${error.message}');
-  }
+  fetchImages(query)
+    .then(data => {
+      hideLoader();
+      if (data.hits.length === 0) {
+        showNotification(
+          'Sorry, there are no images matching your search query. Please try again!',
+          'error'
+        );
+        return;
+      }
+      renderImages(data.hits);
+    })
+    .catch(error => {
+      hideLoader();
+      showNotification('Error: ' + error.message);
+    });
 });
 
 function showLoader() {
